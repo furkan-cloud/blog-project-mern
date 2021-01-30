c13fd8c1f75c1f87ffc20bc83ffc9e8fc3a47a91
furkan-cloud <firstinfirstout2020@gmail.com>
2021-01-31 02:52:15 +0300

    create post added

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,13 +1,24 @@
 import Post from "../models/posts.js";
 
-export const getPosts =  async (req, res) => {
-    try {
-        const posts = await Post.find(); // VERİTABANINDAKİ POSTLARI BUL GETİR
-        res.status(200).json(posts)
-    } catch(error) {
-        res.status(404).json({
-            message: error.message,
-        })
-    }
+export const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find(); // VERİTABANINDAKİ POSTLARI BUL GETİR
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
 
-}
+export const createPost = async (req, res) => {
+  const post = req.body;
+  const newPost = new Post(post);
+  try {
+    await newPost.save();
+  } catch (error) {
+    res.status(409).json({
+      message: error.message,
+    });
+  }
+};
