6b9114856494873b0857f6a43529e6d76516a985
furkan-cloud <firstinfirstout2020@gmail.com>
2021-01-31 15:09:03 +0300

    create post reducer added

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,21 +1,26 @@
 import * as types from "../actions/types";
 
 const initialState = {
-    posts: [],
+  posts: [],
 };
 
 const postReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case types.FETCH_POSTS:
-            return {
-                ...state,
-                posts: action.payload,
-            }
-        default:
-            return {
-                ...state,
-            }
-    }
-}
+  switch (action.type) {
+    case types.FETCH_POSTS:
+      return {
+        ...state,
+        posts: action.payload,
+      };
+    case types.CREATE_POST:
+      return {
+        ...state,
+        posts: [...state.posts, action.payload],
+      };
+    default:
+      return {
+        ...state,
+      };
+  }
+};
 
 export default postReducer;
